0baa2ef98670079b7a54720f8765b71704c20142
Anna <equikav@yandex.ru>
2023-09-25 21:21:08 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// Console.WriteLine(a);
+//  else
+//  Console.WriteLine(b);
+
+//-----------------------------------------------------------------
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+// System.Console.WriteLine(max);
+
+// ----------------------------------------------------------------
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(a % 2);
+// if (0 == a % 2)
+// Console.WriteLine("yes");
+// else
+// Console.WriteLine("no");
+
+// ----------------------------------------------------------------
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for(int i=1;i<=n;i++) 
+//     if (i % 2 == 0)
+// System.Console.Write(i++);
+
+// пришлось лезть в интернет, не знала как решить.
+
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int n = int.Parse(Console.ReadLine()); 
+// for (int i = 1; i <= n; i++)
+//     if (i % 2 == 0)
+//         Console.WriteLine(i);
+
